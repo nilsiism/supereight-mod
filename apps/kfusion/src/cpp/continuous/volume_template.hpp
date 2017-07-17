81e8f1d792bb7553b7f02a2f8654a7da2fb3c3fd
Emanuele <ev314@ic.ac.uk>
2017-07-17 17:02:27 +0100

    Remove half-voxel convention

--- apps/kfusion/src/cpp/continuous/volume_template.hpp
+++ apps/kfusion/src/cpp/continuous/volume_template.hpp
@@ -36,16 +36,15 @@ class VolumeTemplate<FieldType, DynamicStorage, Indexer> {
 
     inline float3 pos(const uint3 & p) const {
       static const float voxelSize = _dim/_size;
-      return make_float3((p.x + 0.5f) * voxelSize, (p.y + 0.5f) * voxelSize, 
-          (p.z + 0.5f) * voxelSize);
+      return make_float3(p.x * voxelSize, p.y * voxelSize, p.z * voxelSize);
     }
 
     void set(const uint3 & pos, const compute_type& d) {}
 
     compute_type operator[](const float3 & p) const {
-      const int3 scaled_pos = make_int3(make_float3((p.x * _size / _dim),
-          (p.y * _size / _dim),
-          (p.z * _size / _dim)));
+      const float inverseVoxelSize = _size/_dim;
+      const int3 scaled_pos = make_int3(make_float3((p.x * inverseVoxelSize),
+          (p.y * inverseVoxelSize), (p.z * inverseVoxelSize)));
       return _map_index.get(scaled_pos.x, scaled_pos.y, scaled_pos.z);
     }
 
@@ -56,9 +55,9 @@ class VolumeTemplate<FieldType, DynamicStorage, Indexer> {
     template <typename FieldSelector>
     float interp(const float3 & pos, FieldSelector select) const {
       const float inverseVoxelSize = _size / _dim;
-      const float3 scaled_pos = make_float3((pos.x * inverseVoxelSize) - 0.5f,
-          (pos.y * inverseVoxelSize) - 0.5f,
-          (pos.z * inverseVoxelSize) - 0.5f);
+      const float3 scaled_pos = make_float3((pos.x * inverseVoxelSize),
+          (pos.y * inverseVoxelSize),
+          (pos.z * inverseVoxelSize));
       return _map_index.interp(scaled_pos, select);
     }
 
@@ -66,9 +65,9 @@ class VolumeTemplate<FieldType, DynamicStorage, Indexer> {
     float3 grad(const float3 & pos, FieldSelector select) const {
 
       const float inverseVoxelSize = _size / _dim;
-      const float3 scaled_pos = make_float3((pos.x * inverseVoxelSize) - 0.5f,
-          (pos.y * inverseVoxelSize) - 0.5f,
-          (pos.z * inverseVoxelSize) - 0.5f);
+      const float3 scaled_pos = make_float3((pos.x * inverseVoxelSize),
+          (pos.y * inverseVoxelSize),
+          (pos.z * inverseVoxelSize));
       return _map_index.grad(scaled_pos, select);
     }
 
--- lib/octree.hpp
+++ lib/octree.hpp
@@ -406,7 +406,6 @@ template <typename T>
 template <typename FieldSelector>
 float Octree<T>::interp(float3 pos, FieldSelector select) const {
   
-  pos = pos - 0.5f;
   const int3 base = make_int3(floorf(pos));
   const float3 factor = fracf(pos);
   const int3 lower = max(base, make_int3(0));
