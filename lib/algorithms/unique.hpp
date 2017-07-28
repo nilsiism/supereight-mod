d8597e4b55a925b5764d2bb5bcac9497ea47b231
emanuelev <ev314@ic.ac.uk>
2017-07-28 13:11:03 +0100

    Fix unique to handle empty array

--- lib/algorithms/unique.hpp
+++ lib/algorithms/unique.hpp
@@ -5,6 +5,7 @@ namespace algorithms {
   template <typename T>
     inline int unique(T* keys, int num_keys){
       int end = 1;
+      if(num_keys < 2) return end;
       for (int i = 1; i < num_keys; ++i){
         if(keys[i] != keys[i-1]){
           keys[end] = keys[i];
