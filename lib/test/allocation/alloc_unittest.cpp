3cbeb1de3e0aa73b504351b91322c95b7a0550b1
emanuelev <ev314@ic.ac.uk>
2017-09-25 10:28:03 +0100

    Fix uint3 to int3 in unit tests

--- lib/test/allocation/alloc_unittest.cpp
+++ lib/test/allocation/alloc_unittest.cpp
@@ -17,7 +17,7 @@ TEST(AllocationTest, EmptySingleVoxel) {
   typedef Octree<float> OctreeF;
   OctreeF oct;
   oct.init(256, 5);
-  const uint3 vox = {25, 65, 127};
+  const int3 vox = {25, 65, 127};
   const uint code = oct.hash(vox.x, vox.y, vox.z); 
   uint allocList[1] = {code};
   const float val = oct.get(vox.x, vox.y, vox.z);
@@ -28,7 +28,7 @@ TEST(AllocationTest, SetSingleVoxel) {
   typedef Octree<float> OctreeF;
   OctreeF oct;
   oct.init(256, 5);
-  const uint3 vox = {25, 65, 127};
+  const int3 vox = {25, 65, 127};
   const uint code = oct.hash(vox.x, vox.y, vox.z); 
   uint allocList[1] = {code};
   oct.allocate(allocList, 1);
