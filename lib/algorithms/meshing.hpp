c4168c094c50812034fa9027f47e72626e28f9ce
emanuelev <ev314@ic.ac.uk>
2018-01-04 15:28:44 +0000

    Refactor meshing code

--- lib/algorithms/meshing.hpp
+++ lib/algorithms/meshing.hpp
@@ -34,122 +34,118 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <octree.hpp>
 #include "edge_tables.h"
 
-template <typename Map, typename FieldSelector>
-inline float3 compute_intersection(const Map& volume, FieldSelector select,
-    const uint3& source, const uint3& dest){
- 
-  const float voxelSize = volume.dim()/volume.size(); 
-  float3 s = make_float3(source.x * voxelSize, source.y * voxelSize, source.z * voxelSize);
-  float3 d = make_float3(dest.x * voxelSize, dest.y * voxelSize, dest.z * voxelSize);
-  float v1 = select(volume.get_fine(source.x, source.y, source.z));
-  float v2 = select(volume.get_fine(dest.x, dest.y, dest.z)); 
-  return s + (0.0 - v1)*(d - s)/(v2-v1);
-  
-}
+namespace meshing {
+  template <typename Map, typename FieldSelector>
+    inline float3 compute_intersection(const Map& volume, FieldSelector select,
+        const uint3& source, const uint3& dest){
+      const float voxelSize = volume.dim()/volume.size(); 
+      float3 s = make_float3(source.x * voxelSize, source.y * voxelSize, source.z * voxelSize);
+      float3 d = make_float3(dest.x * voxelSize, dest.y * voxelSize, dest.z * voxelSize);
+      float v1 = select(volume.get_fine(source.x, source.y, source.z));
+      float v2 = select(volume.get_fine(dest.x, dest.y, dest.z)); 
+      return s + (0.0 - v1)*(d - s)/(v2-v1);
+    }
 
-template <typename Map, typename FieldSelector>
-inline float3 interp_vertexes(const Map& volume, FieldSelector select, 
-    const uint x, const uint y, const uint z, const int edge){
-  switch(edge){
-      case 0:  return compute_intersection(volume, select, make_uint3(x,   y, z),     
-                                           make_uint3(x+1, y, z));
-      case 1:  return compute_intersection(volume, select, make_uint3(x+1, y, z),     
-                                           make_uint3(x+1, y, z+1));
-      case 2:  return compute_intersection(volume, select, make_uint3(x+1, y, z+1),   
-                                           make_uint3(x, y, z+1));
-      case 3:  return compute_intersection(volume, select, make_uint3(x,   y, z),     
-                                           make_uint3(x, y, z+1));
-      case 4:  return compute_intersection(volume, select, make_uint3(x,   y+1, z),   
-                                           make_uint3(x+1, y+1, z));
-      case 5:  return compute_intersection(volume, select, make_uint3(x+1, y+1, z),   
-                                           make_uint3(x+1, y+1, z+1));
-      case 6:  return compute_intersection(volume, select, make_uint3(x+1, y+1, z+1), 
-                                           make_uint3(x, y+1, z+1));
-      case 7:  return compute_intersection(volume, select, make_uint3(x,   y+1, z),   
-                                           make_uint3(x,   y+1, z+1));
+  template <typename Map, typename FieldSelector>
+    inline float3 interp_vertexes(const Map& volume, FieldSelector select, 
+        const uint x, const uint y, const uint z, const int edge){
+      switch(edge){
+        case 0:  return compute_intersection(volume, select, make_uint3(x,   y, z),     
+                     make_uint3(x+1, y, z));
+        case 1:  return compute_intersection(volume, select, make_uint3(x+1, y, z),     
+                     make_uint3(x+1, y, z+1));
+        case 2:  return compute_intersection(volume, select, make_uint3(x+1, y, z+1),   
+                     make_uint3(x, y, z+1));
+        case 3:  return compute_intersection(volume, select, make_uint3(x,   y, z),     
+                     make_uint3(x, y, z+1));
+        case 4:  return compute_intersection(volume, select, make_uint3(x,   y+1, z),   
+                     make_uint3(x+1, y+1, z));
+        case 5:  return compute_intersection(volume, select, make_uint3(x+1, y+1, z),   
+                     make_uint3(x+1, y+1, z+1));
+        case 6:  return compute_intersection(volume, select, make_uint3(x+1, y+1, z+1), 
+                     make_uint3(x, y+1, z+1));
+        case 7:  return compute_intersection(volume, select, make_uint3(x,   y+1, z),   
+                     make_uint3(x,   y+1, z+1));
 
-      case 8:  return compute_intersection(volume, select, make_uint3(x,   y, z),     
-                                           make_uint3(x,   y+1, z));
-      case 9:  return compute_intersection(volume, select, make_uint3(x+1, y, z),     
-                                           make_uint3(x+1, y+1, z));
-      case 10: return compute_intersection(volume, select, make_uint3(x+1, y, z+1),   
-                                           make_uint3(x+1, y+1, z+1));
-      case 11: return compute_intersection(volume, select, make_uint3(x,   y, z+1),   
-                                           make_uint3(x,   y+1, z+1));
+        case 8:  return compute_intersection(volume, select, make_uint3(x,   y, z),     
+                     make_uint3(x,   y+1, z));
+        case 9:  return compute_intersection(volume, select, make_uint3(x+1, y, z),     
+                     make_uint3(x+1, y+1, z));
+        case 10: return compute_intersection(volume, select, make_uint3(x+1, y, z+1),   
+                     make_uint3(x+1, y+1, z+1));
+        case 11: return compute_intersection(volume, select, make_uint3(x,   y, z+1),   
+                     make_uint3(x,   y+1, z+1));
       }
-  return make_float3(0);
-}
+      return make_float3(0);
+    }
 
-template <typename T, typename FieldSelector>
-uint8_t compute_index(const uint x, const uint y, const uint z,
-                              VoxelBlock<T> *leaf, FieldSelector select){
-   uint8_t index = 0;
-   typename Octree<FieldType>::compute_type points[8];
-   points[0] = leaf->data(make_int3(x, y, z)); 
-   points[1] = leaf->data(make_int3(x+1, y, z));
-   points[2] = leaf->data(make_int3(x+1, y, z+1));
-   points[3] = leaf->data(make_int3(x, y, z+1));
-   points[4] = leaf->data(make_int3(x, y+1, z));
-   points[5] = leaf->data(make_int3(x+1, y+1, z));
-   points[6] = leaf->data(make_int3(x+1, y+1, z+1));
-   points[7] = leaf->data(make_int3(x, y+1, z+1));
+  template <typename FieldType, typename PointT>
+    inline void gather_points( const VoxelBlock<FieldType>* cached, PointT points[8], 
+        const int x, const int y, const int z) {
+      points[0] = cached->data(make_int3(x, y, z)); 
+      points[1] = cached->data(make_int3(x+1, y, z));
+      points[2] = cached->data(make_int3(x+1, y, z+1));
+      points[3] = cached->data(make_int3(x, y, z+1));
+      points[4] = cached->data(make_int3(x, y+1, z));
+      points[5] = cached->data(make_int3(x+1, y+1, z));
+      points[6] = cached->data(make_int3(x+1, y+1, z+1));
+      points[7] = cached->data(make_int3(x, y+1, z+1));
+    }
 
-   if(points[0].y == 0.f) return 0;
-   if(points[1].y == 0.f) return 0;
-   if(points[2].y == 0.f) return 0;
-   if(points[3].y == 0.f) return 0;
-   if(points[4].y == 0.f) return 0;
-   if(points[5].y == 0.f) return 0;
-   if(points[6].y == 0.f) return 0;
-   if(points[7].y == 0.f) return 0;
+  template <typename FieldType, template <typename FieldType> class MapT,
+           typename PointT>
+             inline void gather_points(const MapT<FieldType>& volume, PointT points[8], 
+                 const int x, const int y, const int z) {
+               points[0] = volume.get_fine(x, y, z); 
+               points[1] = volume.get_fine(x+1, y, z);
+               points[2] = volume.get_fine(x+1, y, z+1);
+               points[3] = volume.get_fine(x, y, z+1);
+               points[4] = volume.get_fine(x, y+1, z);
+               points[5] = volume.get_fine(x+1, y+1, z);
+               points[6] = volume.get_fine(x+1, y+1, z+1);
+               points[7] = volume.get_fine(x, y+1, z+1);
+             }
 
-   if(select(points[0]) < 0.f) index |= 1;
-   if(select(points[1]) < 0.f) index |= 2;
-   if(select(points[2]) < 0.f) index |= 4;
-   if(select(points[3]) < 0.f) index |= 8;
-   if(select(points[4]) < 0.f) index |= 16;
-   if(select(points[5]) < 0.f) index |= 32;
-   if(select(points[6]) < 0.f) index |= 64;
-   if(select(points[7]) < 0.f) index |= 128;
-   return index;
-}
+  template <typename FieldType, template <typename FieldType> class MapT,
+           typename FieldSelector>
+             uint8_t compute_index(const MapT<FieldType>& volume, 
+                 const VoxelBlock<FieldType>* cached, FieldSelector select,
+                 const uint x, const uint y, const uint z){
 
-template <typename Map, typename FieldSelector>
-uint8_t compute_index_at_border(const Map& volume, FieldSelector select, 
-    const uint x, const uint y, const uint z){
-   uint8_t index = 0;
-   // constexpr auto& volume.get = Octree<T>::volume.get;
-   typename Map::compute_type points[8];
-   points[0] = volume.get_fine(x, y, z); 
-   points[1] = volume.get_fine(x+1, y, z);
-   points[2] = volume.get_fine(x+1, y, z+1);
-   points[3] = volume.get_fine(x, y, z+1);
-   points[4] = volume.get_fine(x, y+1, z);
-   points[5] = volume.get_fine(x+1, y+1, z);
-   points[6] = volume.get_fine(x+1, y+1, z+1);
-   points[7] = volume.get_fine(x, y+1, z+1);
+               unsigned int blockSize =  VoxelBlock<FieldType>::side;
+               unsigned int local = ((x % blockSize == blockSize - 1) << 2) | 
+                 ((y % blockSize == blockSize - 1) << 1) |
+                 ((z % blockSize) == blockSize - 1);
 
-   if(points[0].y == 0.f) return 0;
-   if(points[1].y == 0.f) return 0;
-   if(points[2].y == 0.f) return 0;
-   if(points[3].y == 0.f) return 0;
-   if(points[4].y == 0.f) return 0;
-   if(points[5].y == 0.f) return 0;
-   if(points[6].y == 0.f) return 0;
-   if(points[7].y == 0.f) return 0;
+               typename MapT<FieldType>::compute_type points[8];
+               if(!local) gather_points(cached, points, x, y, z);
+               else gather_points(volume, points, x, y, z);
 
-   if(select(points[0]) < 0.f) index |= 1;
-   if(select(points[1]) < 0.f) index |= 2;
-   if(select(points[2]) < 0.f) index |= 4;
-   if(select(points[3]) < 0.f) index |= 8;
-   if(select(points[4]) < 0.f) index |= 16;
-   if(select(points[5]) < 0.f) index |= 32;
-   if(select(points[6]) < 0.f) index |= 64;
-   if(select(points[7]) < 0.f) index |= 128;
-   return index;
-}
-bool checkVertex(const float3 v, const int dim){
-  return (v.x <= 0 || v.y <=0 || v.z <= 0 || v.x > dim || v.y > dim || v.z > dim);
+               uint8_t index = 0;
+               if(points[0].y == 0.f) return 0;
+               if(points[1].y == 0.f) return 0;
+               if(points[2].y == 0.f) return 0;
+               if(points[3].y == 0.f) return 0;
+               if(points[4].y == 0.f) return 0;
+               if(points[5].y == 0.f) return 0;
+               if(points[6].y == 0.f) return 0;
+               if(points[7].y == 0.f) return 0;
+
+               if(select(points[0]) < 0.f) index |= 1;
+               if(select(points[1]) < 0.f) index |= 2;
+               if(select(points[2]) < 0.f) index |= 4;
+               if(select(points[3]) < 0.f) index |= 8;
+               if(select(points[4]) < 0.f) index |= 16;
+               if(select(points[5]) < 0.f) index |= 32;
+               if(select(points[6]) < 0.f) index |= 64;
+               if(select(points[7]) < 0.f) index |= 128;
+               return index;
+             }
+
+  inline bool checkVertex(const float3 v, const int dim){
+    return (v.x <= 0 || v.y <=0 || v.z <= 0 || v.x > dim || v.y > dim || v.z > dim);
+  }
+
 }
 namespace algorithms {
   template <typename FieldType, typename FieldSelector, typename TriangleType>
@@ -157,6 +153,7 @@ namespace algorithms {
         std::vector<TriangleType>& triangles)
     {
 
+      using namespace meshing;
       std::stringstream points, polygons;
       std::vector<VoxelBlock<FieldType>*> blocklist;
       std::mutex lck;
@@ -178,11 +175,9 @@ namespace algorithms {
           for(y = leaf->coordinates().y; y < ybound; y++){
             for(z = leaf->coordinates().z; z < zbound; z++){
 
-              uint8_t index = (x == xbound-1 || y == ybound-1 || z == zbound-1 ) ?
-                compute_index_at_border(volume, select, x, y, z) :
-                compute_index(x, y, z, leaf, select);
+              uint8_t index = meshing::compute_index(volume, leaf, select, x, y, z);
 
-              int * edges = triTable[index]; 
+                int * edges = triTable[index]; 
               for(unsigned int e = 0; edges[e] != -1 && e < 16; e += 3){
                 float3 v1 = interp_vertexes(volume, select, x, y, z, edges[e]);
                 float3 v2 = interp_vertexes(volume, select, x, y, z, edges[e+1]);
