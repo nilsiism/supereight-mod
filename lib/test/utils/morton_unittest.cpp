6c28e5b322779c0092e7ba0da3fa93c191513f7c
Emanuele <ev314@ic.ac.uk>
2018-02-28 15:05:59 +0000

    Add test case to morton_unittest.cpp

--- lib/test/utils/morton_unittest.cpp
+++ lib/test/utils/morton_unittest.cpp
@@ -6,7 +6,6 @@
 
 TEST(MortonCoding, RandomInts) {
 
-
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<unsigned int> dis(0, 4096);
@@ -18,6 +17,21 @@ TEST(MortonCoding, RandomInts) {
     ASSERT_EQ(decoded.x, vox.x);
     ASSERT_EQ(decoded.y, vox.y);
     ASSERT_EQ(decoded.z, vox.z);
+  }
+
+}
+
+TEST(MortonCoding, ExhaustiveTest) {
+
+  for(unsigned int z = 2048; z < 4096; ++z)
+    for(unsigned int y = 2048; y < 2050; ++y)
+      for(unsigned int x = 0; x < 4096; ++x){
+        const uint3 vox = {x, y, z};
+        const morton_type code = compute_morton(vox.x, vox.y, vox.z);
+        const uint3 decoded = unpack_morton(code);
+        ASSERT_EQ(decoded.x, vox.x);
+        ASSERT_EQ(decoded.y, vox.y);
+        ASSERT_EQ(decoded.z, vox.z);
   }
 
 }
