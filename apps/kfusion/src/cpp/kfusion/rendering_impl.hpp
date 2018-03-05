f4534a0efc8d5f4b9e5de872a83d7e79cced224d
Emanuele <ev314@ic.ac.uk>
2018-03-05 16:42:57 +0000

    Fix volume nn sampling operator

--- apps/kfusion/src/cpp/kfusion/rendering_impl.hpp
+++ apps/kfusion/src/cpp/kfusion/rendering_impl.hpp
@@ -39,7 +39,7 @@ float4 raycast(const Volume<SDF>& volume, const uint2 pos, const Matrix4 view,
     float f_tt = 0;
     if (f_t > 0) { // ups, if we were already in it, then don't render anything here
       for (; t < tfar; t += stepsize) {
-        Volume<SDF>::compute_type data = volume[position];
+        Volume<SDF>::compute_type data = volume.get(position);
         if(data.y < 0){
           stepsize = largestep;
           position += stepsize*direction;
