75eda921c1df4cbccb4703d90f8d5b09a8c7f7a1
Emanuele <ev314@ic.ac.uk>
2018-03-22 15:52:46 +0000

    Initialise test function in interp unit test

--- lib/test/interp/interpolation_unittest.cpp
+++ lib/test/interp/interpolation_unittest.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "octree.hpp"
 #include "math_utils.h"
 #include "gtest/gtest.h"
@@ -15,24 +16,65 @@ struct voxel_traits<testT> {
   }
 };
 
+float test_fun(float x, float y, float z) {
+  return sq(z) + std::sin(2*x + y);
+}
+
 class InterpolationTest : public ::testing::Test {
   protected:
     virtual void SetUp() {
-      oct_.init(512, 5);
-      const int3 blocks[10] = {{56, 12, 254}, {87, 32, 423}, {128, 128, 128},
-      {136, 128, 128}, {128, 136, 128}, {136, 136, 128}, 
-      {128, 128, 136}, {136, 128, 136}, {128, 136, 136}, {136, 136, 136}};
-      octlib::key_t alloc_list[10];
-      for(int i = 0; i < 10; ++i) {
-        alloc_list[i] = oct_.hash(blocks[i].x, blocks[i].y, blocks[i].z);
-      }
-      oct_.allocate(alloc_list, 10);
+      unsigned size = 256;
+      float dim = 5.f;
+      oct_.init(size, dim); // 5 meters
+
+      const unsigned center = 2.5f;
+      const unsigned radius = center + 0.5f; 
+
+      const float voxelsize = oct_.dim()/oct_.size();
+      const float inverse_voxelsize = 1.f/voxelsize;
+      unsigned leaf_level = log2(size) - log2(Octree<testT>::blockSide);
+      for(float z = center - radius; z < center + radius; ++z)
+        for(float y = center - radius; y < center + radius; ++y)
+          for(float x = center - radius; x < center + radius; ++x) {
+            const int3 vox =  make_int3(x * inverse_voxelsize, 
+                y * inverse_voxelsize, z * inverse_voxelsize);
+            alloc_list.push_back(oct_.hash(vox.x, vox.y, vox.z, leaf_level));
+          }
+      oct_.alloc_update(alloc_list.data(), alloc_list.size());
     }
 
   typedef Octree<testT> OctreeF;
   OctreeF oct_;
+  std::vector<octlib::key_t> alloc_list;
 };
 
 TEST_F(InterpolationTest, Init) {
-  EXPECT_EQ(oct_.get(137, 138, 130), voxel_traits<testT>::initValue());
+
+  auto initialise = [](auto& handler, const int3& v) {
+    handler.set(test_fun(v.x, v.y, v.z));
+  }; 
+  iterators::functor::axis_aligned<testT, Octree, decltype(initialise)> 
+    funct(oct_, initialise);
+  funct.apply();
+
+  auto test = [](auto& handler, const int3& v) {
+    auto data = handler.get();
+    ASSERT_EQ(data, test_fun(v.x, v.y, v.z));
+  }; 
+  iterators::functor::axis_aligned<testT, Octree, decltype(test)> 
+    funct_test(oct_, test);
+  funct_test.apply();
 }
+
+// TEST_F(InterpolationTest, InterpAtPoints) {
+// 
+//   auto test = [this](auto& handler, const int3& v) {
+//     auto data = handler.get();
+//     float interpolated = oct_.interp(make_float3(v.x, v.y, v.z), [](const auto& val){ return val; });
+//     ASSERT_EQ(data, interpolated);
+//   }; 
+// 
+//   iterators::functor::axis_aligned<testT, Octree, decltype(test)> 
+//     funct_test(oct_, test);
+//   funct_test.apply();
+// }
