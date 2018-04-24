6d604369f9aacc8ec207273714bd5213ed28d11d
emanuelev <ev314@ic.ac.uk>
2018-04-24 12:19:20 +0100

    Add a function to compute octant's far corner
    The far corner of an octant is the corner which is not shared with any
    of his siblings. The function far_cornern computes the coordinates of
    such corner given an octant, it's level and the maximum tree depth.

--- lib/octant_ops.hpp
+++ lib/octant_ops.hpp
@@ -3,6 +3,7 @@
 #include "utils/morton_utils.hpp"
 #include "octree_defines.h"
 #include <bitset>
+#include <iostream>
 
 /*
  * Algorithm 5 of p4est paper: https://epubs.siam.org/doi/abs/10.1137/100791634
@@ -10,7 +11,7 @@
 inline uint3 face_neighbour(const octlib::key_t o, 
     const unsigned int face, const unsigned int l, 
     const unsigned int max_depth) {
-  uint3 coords = unpack_morton(o);
+  uint3 coords = unpack_morton(o & ~SCALE_MASK);
   const unsigned int side = 1 << (max_depth - l); 
   coords.x = coords.x + ((face == 0) ? -side : (face == 1) ? side : 0);
   coords.y = coords.y + ((face == 2) ? -side : (face == 3) ? side : 0);
@@ -39,12 +40,34 @@ inline octlib::key_t parent(const octlib::key_t& octant, const int max_depth) {
   return (octant & MASK[idx]) | level;
 }
 
+/*
+ * \brief Computes the octants's id in its local brotherhood
+ * \param octant
+ * \param level of octant 
+ * \param max_depth max depth of the tree on which the octant lives
+ */
 inline int child_id(octlib::key_t octant, const int level, 
     const int max_depth) {
   int shift = max_depth - level;
   octant = octant >> shift*3;
   int idx = (octant & 0x01) | (octant & 0x02) | (octant & 0x04);
   return idx;
+}
+
+/*
+ * \brief Computes the octants's corner which is not shared with its siblings
+ * \param octant
+ * \param level of octant 
+ * \param max_depth max depth of the tree on which the octant lives
+ */
+inline int3 far_corner(const octlib::key_t octant, const int level, 
+    const int max_depth) {
+  const unsigned int side = 1 << (max_depth - level); 
+  const int idx = child_id(octant, level, max_depth);
+  const uint3 coordinates = unpack_morton(octant & ~SCALE_MASK);
+  return make_int3(coordinates.x + (idx & 1) * side,
+                   coordinates.y + ((idx & 2) >> 1) * side,
+                   coordinates.z + ((idx & 4) >> 2) * side);
 }
 #endif
 
--- lib/test/octree/octree_unittest.cpp
+++ lib/test/octree/octree_unittest.cpp
@@ -45,3 +45,64 @@ TEST(Octree, OctantParent) {
   ASSERT_EQ(2, p & SCALE_MASK);
   ASSERT_EQ(p & ~SCALE_MASK, compute_morton(64, 64, 128));
 }
+
+TEST(Octree, FarCorner) {
+  const int max_depth = 5;
+  const int level = 2;
+
+  /* First child */
+  const octlib::key_t cell0 = compute_morton(16, 16, 16);
+  const int3 fc0 = far_corner(cell0, level, max_depth);
+  ASSERT_EQ(fc0.x, 16);
+  ASSERT_EQ(fc0.y, 16);
+  ASSERT_EQ(fc0.z, 16);
+
+  /* Second child */
+  const octlib::key_t cell1 = compute_morton(24, 16, 16);
+  const int3 fc1 = far_corner(cell1, level, max_depth);
+  ASSERT_EQ(fc1.x, 32);
+  ASSERT_EQ(fc1.y, 16);
+  ASSERT_EQ(fc1.z, 16);
+
+  /* Third child */
+  const octlib::key_t cell2 = compute_morton(16, 24, 16);
+  const int3 fc2 = far_corner(cell2, level, max_depth);
+  ASSERT_EQ(fc2.x, 16);
+  ASSERT_EQ(fc2.y, 32);
+  ASSERT_EQ(fc2.z, 16);
+
+  /* Fourth child */
+  const octlib::key_t cell3 = compute_morton(24, 24, 16);
+  const int3 fc3 = far_corner(cell3, level, max_depth);
+  ASSERT_EQ(fc3.x, 32);
+  ASSERT_EQ(fc3.y, 32);
+  ASSERT_EQ(fc3.z, 16);
+
+  /* Fifth child */
+  const octlib::key_t cell4 = compute_morton(24, 24, 16);
+  const int3 fc4 = far_corner(cell4, level, max_depth);
+  ASSERT_EQ(fc4.x, 32);
+  ASSERT_EQ(fc4.y, 32);
+  ASSERT_EQ(fc4.z, 16);
+
+  /* sixth child */
+  const octlib::key_t cell5 = compute_morton(16, 16, 24);
+  const int3 fc5 = far_corner(cell5, level, max_depth);
+  ASSERT_EQ(fc5.x, 16);
+  ASSERT_EQ(fc5.y, 16);
+  ASSERT_EQ(fc5.z, 32);
+
+  /* seventh child */
+  const octlib::key_t cell6 = compute_morton(24, 16, 24);
+  const int3 fc6 = far_corner(cell6, level, max_depth);
+  ASSERT_EQ(fc6.x, 32);
+  ASSERT_EQ(fc6.y, 16);
+  ASSERT_EQ(fc6.z, 32);
+
+  /* eight child */
+  const octlib::key_t cell7 = compute_morton(24, 24, 24);
+  const int3 fc7 = far_corner(cell7, level, max_depth);
+  ASSERT_EQ(fc7.x, 32);
+  ASSERT_EQ(fc7.y, 32);
+  ASSERT_EQ(fc7.z, 32);
+}
