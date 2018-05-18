383e531f2cd6fcccc4e3f095af68c895d8acf35f
emanuelev <ev314@ic.ac.uk>
2018-05-18 14:07:53 +0100

    Raycast for rendering only if necessary

--- apps/kfusion/src/cpp/rendering.cpp
+++ apps/kfusion/src/cpp/rendering.cpp
@@ -141,45 +141,56 @@ template <typename T>
 void renderVolumeKernel(const Volume<T>& volume, uchar4* out, const uint2 depthSize, const Matrix4 view, 
     const float nearPlane, const float farPlane, const float mu,
 		const float step, const float largestep, const float3 light,
-		const float3 ambient, bool ) {
+		const float3 ambient, 
+    bool render, const float3 * vertex, const float3 * normal) {
 	TICK();
   unsigned int y;
 #pragma omp parallel for shared(out), private(y)
 	for (y = 0; y < depthSize.y; y++) {
 		for (unsigned int x = 0; x < depthSize.x; x++) {
 			const uint2 pos = make_uint2(x, y);
-      const Eigen::Vector3f dir = 
-        (to_sophus(view).so3() * Eigen::Vector3f(x, y, 1.f)).normalized();
-      const float3 transl = get_translation(view);
-      octlib::ray_iterator<typename Volume<T>::field_type> ray(volume._map_index, 
-          Eigen::Vector3f(transl.x, transl.y, transl.z), dir, nearPlane, 
-          farPlane);
-      ray.next();
-      const float t_min = ray.tmin(); /* Get distance to the first intersected block */
-      const float4 hit = t_min > 0.f ? 
-        raycast(volume, pos, view, t_min*volume._dim/volume._size, 
-          farPlane, mu, step, largestep) : make_float4(0.f);
-			if (hit.w > 0) {
-				const float3 test = make_float3(hit);
-        Eigen::Vector3f tmp = volume.grad(make_float3(hit), 
-            [](const auto& val){ return val.x; });
-				float3 surfNorm = make_float3(tmp(0), tmp(1), tmp(2));
-				if (length(surfNorm) > 0) {
-					const float3 diff = (std::is_same<T, SDF>::value ?
-              normalize(light - test) : normalize(test - light));
-					const float dir = fmaxf(dot(normalize(surfNorm), diff),
-							0.f);
-					const float3 col = clamp(make_float3(dir) + ambient, 0.f,
-							1.f) * 255;
-					out[x + depthSize.x*y] = make_uchar4(col.x, col.y, col.z, 0); // The forth value is a padding to align memory
-				} else {
-					out[x + depthSize.x*y] = make_uchar4(0, 0, 0, 0); // The forth value is a padding to align memory
-				}
-			} else {
-				out[x + depthSize.x*y] = make_uchar4(0, 0, 0, 0); // The forth value is a padding to align memory
-			}
-		}
-	}
-	TOCK("renderVolumeKernel", depthSize.x * depthSize.y);
+      float4 hit;
+      float3 test, surfNorm;
+
+      if(render) {
+        const Eigen::Vector3f dir = 
+          (to_sophus(view).so3() * Eigen::Vector3f(x, y, 1.f)).normalized();
+        const float3 transl = get_translation(view);
+        octlib::ray_iterator<typename Volume<T>::field_type> ray(volume._map_index, 
+            Eigen::Vector3f(transl.x, transl.y, transl.z), dir, nearPlane, 
+            farPlane);
+        ray.next();
+        const float t_min = ray.tmin(); /* Get distance to the first intersected block */
+        hit = t_min > 0.f ? 
+          raycast(volume, pos, view, t_min*volume._dim/volume._size, 
+              farPlane, mu, step, largestep) : make_float4(0.f);
+        if (hit.w > 0) {
+          test = make_float3(hit);
+          Eigen::Vector3f tmp = volume.grad(make_float3(hit), 
+              [](const auto& val){ return val.x; });
+          surfNorm = make_float3(tmp(0), tmp(1), tmp(2));
+        } else {
+          out[x + depthSize.x*y] = make_uchar4(0, 0, 0, 0); // The forth value is a padding to align memory
+        }
+      }
+      else {
+        test = vertex[x + depthSize.x*y];
+        surfNorm = normal[x + depthSize.x*y];
+      }
+
+      if (length(surfNorm) > 0) {
+        const float3 diff = (std::is_same<T, SDF>::value ?
+            normalize(light - test) : normalize(test - light));
+        const float dir = fmaxf(dot(normalize(surfNorm), diff),
+            0.f);
+        const float3 col = clamp(make_float3(dir) + ambient, 0.f,
+            1.f) * 255;
+        out[x + depthSize.x*y] = make_uchar4(col.x, col.y, col.z, 0); // The forth value is a padding to align memory
+      } else {
+        out[x + depthSize.x*y] = make_uchar4(0, 0, 0, 0); // The forth value is a padding to align memory
+      }
+  }
+}
+TOCK("renderVolumeKernel", depthSize.x * depthSize.y);
 }
 
