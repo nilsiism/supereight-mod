98f80170d07889f3742b437a306f22bf691be2f0
Emanuele <ev314@ic.ac.uk>
2018-05-21 06:15:21 +0100

    Fix bfusion block skipping

--- apps/kfusion/src/cpp/bfusion/rendering_impl.hpp
+++ apps/kfusion/src/cpp/bfusion/rendering_impl.hpp
@@ -40,6 +40,10 @@ float4 raycast(const Volume<BFusion>& volume, const uint2 pos, const Matrix4 vie
     if (f_t <= SURF_BOUNDARY) { 
       for (; t < tfar; t += stepsize) {
         Volume<BFusion>::compute_type data = volume.get(origin + direction * t);
+        if(data.y == 0){
+          t += stepsize * 6.0f;
+          continue;
+        }
         if(data.x > -100.f && data.y > 0.f){
           f_tt = volume.interp(origin + direction * t, select_occupancy);
         }
