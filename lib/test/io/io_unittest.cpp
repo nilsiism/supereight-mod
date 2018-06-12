c508478e682d7f921627cb57460db77ace40d212
Emanuele Vespa <e.vespa14@imperial.ac.uk>
2018-06-12 18:00:30 +0100

    Implement node serialisation
    This commit implements Node<T> serialisation and deserialisation. We do
    not serialise the pointers to children as this information will be
    reconstructed when loading the whole tree.

---
+++ lib/test/io/io_unittest.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <string>
+#include "se_serialise.hpp"
+#include "node.hpp"
+#include "gtest/gtest.h"
+
+typedef float testT;
+template <>
+struct voxel_traits<testT> {
+  typedef float value_type;
+  static inline value_type empty(){ return 0.f; }
+  static inline value_type initValue(){ return 10.f; }
+};
+
+
+TEST(SerialiseUnitTest, WriteReadNode) {
+  std::string filename = "test.bin";
+  {
+    std::ofstream os (filename, std::ios::binary); 
+    se::Node<testT> octant;
+    octant.code = 24;
+    octant.side = 256;
+    se::internal::serialise(os, octant);
+  }
+
+  {
+    std::ifstream is(filename, std::ios::binary);
+    se::Node<testT> octant;
+    se::internal::deserialise(octant, is);
+    std::cout << "Read: " << octant.code << ", " << octant.side << " ";
+    for(int i = 0; i < 8; ++i)
+      std::cout << octant.value_[i] << " ";
+  }
+}
+
