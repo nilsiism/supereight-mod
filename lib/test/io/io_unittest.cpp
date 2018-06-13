ee6bf2f497f3a1ed104eeb49603d0d783e76e330
emanuelev <ev314@ic.ac.uk>
2018-06-13 12:24:57 +0100

    Use asserts in io_unittest.cpp

--- lib/test/io/io_unittest.cpp
+++ lib/test/io/io_unittest.cpp
@@ -12,6 +12,16 @@ struct voxel_traits<testT> {
   static inline value_type initValue(){ return 10.f; }
 };
 
+class Occupancy;
+template <>
+struct voxel_traits<Occupancy> {
+  typedef struct { 
+    float x;
+    double y;
+  } value_type;
+  static inline value_type empty(){ return {0.f, 0.}; }
+  static inline value_type initValue(){ return {1.f, 0.}; }
+};
 
 TEST(SerialiseUnitTest, WriteReadNode) {
   std::string filename = "test.bin";
@@ -20,6 +30,8 @@ TEST(SerialiseUnitTest, WriteReadNode) {
     se::Node<testT> octant;
     octant.code = 24;
     octant.side = 256;
+    for(int i = 0; i < 8; ++i)
+      octant.value_[i] =  5.f;
     se::internal::serialise(os, octant);
   }
 
@@ -27,9 +39,10 @@ TEST(SerialiseUnitTest, WriteReadNode) {
     std::ifstream is(filename, std::ios::binary);
     se::Node<testT> octant;
     se::internal::deserialise(octant, is);
-    std::cout << "Read: " << octant.code << ", " << octant.side << " ";
+    ASSERT_EQ(octant.code, 24);
+    ASSERT_EQ(octant.side, 256);
     for(int i = 0; i < 8; ++i)
-      std::cout << octant.value_[i] << " ";
+      ASSERT_EQ(octant.value_[i], 5.f);
   }
 }
 
@@ -49,10 +62,36 @@ TEST(SerialiseUnitTest, WriteReadBlock) {
     std::ifstream is(filename, std::ios::binary);
     se::VoxelBlock<testT> octant;
     se::internal::deserialise(octant, is);
-    std::cout << "Read: " << octant.code << ", " << octant.side << " "
-      << ", coords:\n " << octant.coordinates();
+    ASSERT_EQ(octant.code, 24);
+    ASSERT_TRUE(octant.coordinates() == Eigen::Vector3i(40, 48, 52));
     for(int i = 0; i < 512; ++i)
       ASSERT_EQ(octant.data(i), 5.f);
+  }
+}
+
+TEST(SerialiseUnitTest, WriteReadBlockStruct) {
+  std::string filename = "test.bin";
+  {
+    std::ofstream os (filename, std::ios::binary); 
+    se::VoxelBlock<Occupancy> octant;
+    octant.code = 24;
+    octant.coordinates(Eigen::Vector3i(40, 48, 52));
+    for(int i = 0; i < 512; ++i)
+      octant.data(i, {5.f, 2.});
+    se::internal::serialise(os, octant);
+  }
+
+  {
+    std::ifstream is(filename, std::ios::binary);
+    se::VoxelBlock<Occupancy> octant;
+    se::internal::deserialise(octant, is);
+    ASSERT_EQ(octant.code, 24);
+    ASSERT_TRUE(octant.coordinates() == Eigen::Vector3i(40, 48, 52));
+    for(int i = 0; i < 512; ++i) {
+      auto data = octant.data(i);
+      ASSERT_EQ(data.x, 5.f);
+      ASSERT_EQ(data.y, 2.);
+    }
   }
 }
 
